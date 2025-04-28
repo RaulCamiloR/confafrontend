82aefd1473f5b2f6f372aeb8c73000d26827a18b
RaulCamiloR <camilo.rodriguez@cloudhesive.com>
2025-04-28 16:04:55 -0500

    comment

--- app/api/create-template/route.ts
+++ app/api/create-template/route.ts
@@ -14,6 +14,8 @@ export async function POST(request: Request) {
             channel
         }
 
+        // hi
+
         console.log({params})
 
         const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/email/templates`, params, { withCredentials: true })
